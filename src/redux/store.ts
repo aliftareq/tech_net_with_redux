248e6571b06c34ac4cc6797f12c6a219dc366d0b
alif <aliftareq@gmail.com>
2023-09-13 15:06:20 +0600

    rtk with api added and login and signup modified

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,9 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
 import cartReducer from './features/cart/cartSlice';
+import productReducer from './features/products/productSlice';
+import { Api } from './Api/apiSlice';
 const store = configureStore({
   reducer: {
     cart: cartReducer,
+    productFilter: productReducer,
+    [Api.reducerPath]: Api.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(Api.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
